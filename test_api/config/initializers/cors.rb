4c522068b0738984a717f9f7e538768d59e6400b
熊谷洸介(Kousuke Kumagai) <kumagaikousuke1995@gmail.com>
2019-10-14 23:01:56 +0900

    todoをかいた

--- test_api/config/initializers/cors.rb
+++ test_api/config/initializers/cors.rb
@@ -7,8 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    # TODO:プロダクトリリースするときにきちんと設定する
     origins '*'
 
+    # TODO:プロダクトリリースするときにきちんと設定する
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
