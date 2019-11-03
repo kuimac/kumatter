47ec6096f438a8cbaf6d22b3b7e18eb7ff4659f1
熊谷洸介(Kousuke Kumagai) <kumagaikousuke1995@gmail.com>
2019-11-04 05:58:09 +0900

    テストが通るようにした

--- angularApp/test-web/src/app/app.component.spec.ts
+++ angularApp/test-web/src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
       ],
+      imports: [
+        HttpClientModule],
     }).compileComponents();
   }));
 
@@ -19,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'test-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('test-web');
+    expect(app.title).toEqual('test');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to test-web!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to test!');
   });
 });
